96d7fdb9379391067cd7d611be6043d175b41a64
Xushi <xushi.official@gmail.com>
2026-02-22 22:27:16 +0100

    Updated the help texts

--- DTD-modules/dtd.py
+++ DTD-modules/dtd.py
@@ -20,9 +20,16 @@ This time can be used to perform a few activities, such as completing a random t
 ===
 **Commands**:
 `!dtd job "[skill1]" "[skill2]"` - Random Job: Some people around the city might find themselves in need of a hand for a one-off job, this may include finding a lost object or commissioning. The pay is directly related to the rank of the adventurers.
+> `skill1`: acrobatics, athletics, stealth.
+> `skill2`: animal handling, deception, intimidation, investigation, nature, perception.
+
 `!dtd train` - Training: As all adventurers, training is an important part of the job, hence why gathering experience from such training is important. It may simply be a training related to evading attacks to a spar.
 
-`skill1`: acrobatics, athletics, stealth.
-`skill2`: animal handling, deception, intimidation, investigation, nature, perception."""
+`!dtd med "[skill1]" "[skill2]"` - Medic Job: The guild is in constant need of people for help, as the numbers of injured seems to pile up day by day, as such the guild calls upon the adventurers to help the injured people as one-off job. Of course the guild will pay based on the rank of the adventurer.
+> `skill1`: medicine, nature, investigation, survival, animal handling.
+> `skill2`: sleight of hand, insight, history, persuasion, perception."""
         -footer "!dtd help | Athanor"
     '''
+
+VALID_SKILLS1 = ["medicine", "nature", "investigation", "survival", "animalHandling"]
+VALID_SKILLS2 = ["sleightOfHand", "insight", "history", "persuasion", "perception"]
--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 This repository contains the python files for downtime activities in draconic.
 
-## !alifestyle
+## Usage
 
-The `!alifestyle.py` file contains the script in draconic for the `!alifestyle` command, which creates the necessary counters for downtime activities, though it is currently not used by the other commands.
-
-## !job
-
-The `!job.py` file contains the script in draconic for the `!MONEY` command, it takes in two skills and is limited to be used once a day and carries a built-in exhaustion system so that players shouldn't use it everyday and take a break every 5 days at the very least.
+For building the alias:
+- Create the modules under `DTD-modules/` you need
+- Add modules and their build order in `build-order.json`
+- Run the build script (`python build.py`)
+- Copy paste the alias in `dist/` to your alias on avrae dashboard.
 
 ## TODO list
 
@@ -20,8 +20,10 @@ The `!job.py` file contains the script in draconic for the `!MONEY` command, it 
 - [x] Implementing exhaustion system
 - [ ] Add tool checks for `!dtd job`
 - [ ] Add multiple job types:
-    - [ ] Healing DTD
+    - [x] Healing DTD
     - [ ] Eotorath DTD
+        - [x] The actual DTD
+        - [ ] Adding progress bar
 - [ ] DDB friendly alias
 - [ ] PTW/HRW 2.0
 - [ ] All DTD into 1 alias python script
